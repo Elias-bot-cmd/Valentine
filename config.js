ef4126ce49c67d92735ddeccc4a3fb265e1b2180
Elias-bot-cmd <sikariio1234@gmail.com>
2026-02-14 01:30:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,35 +3,35 @@
 // ============================================
 
 const CONFIG = { 
-    valentineName: "Samira",
+    valentineName: "Juliana GORDIX",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Quieres ser mi san valetin? 🤍 🌹",
+    pageTitle: "Quieres ser mi san valetin? 💞 🌹",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['🌹', '❣️', '🤍', '💗', '💓'],  // Heart emojis
-        bears: ['🦦', '🐥']                       // Cute bear emojis
+        bears: ['💖', '🤍']                       // Cute bear emojis
     },
 
     // Questions and answers
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Te gusto mucho??",                                    // First interaction
+            text: "Te gustan mis peos?",                                    // First interaction
             yesBtn: "Si",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "No me gustas, me encantas.🤍"           // Secret hover message
+            secretAnswer: "No me gustas, me encantan y tambien con patas hediondas. "           // Secret hover message
         },
         second: {
             text: "Cuanto me amas?",                          // For the love meter
             startText: "Asi de mucho?",                                   // Text before the percentage
-            nextBtn: "Poco? 💔"                                         // Text for the next button
+            nextBtn: "ENSERIO?!!!"                                         // Text for the next button
         },
         third: {
-            text: "Quieres ser mi San Valetin este 14 de Feb Del 2026? 🌹", // The big question!
+            text: "Quieres ser mi San Valentin este 14 de Feb Del 2026? 🌹", // The big question!
             yesBtn: "SISIISI",                                             // Text for "Yes" button
             noBtn: "No wakala"                                                 // Text for "No" button
         }
@@ -47,9 +47,9 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "JEJEJe, Soy la persona mas afortunada de tenerte 😈😈🤍🤍",
-        message: "No puedo darte un regalo precensial pero tu sabes que hago lo que puedo, espera a que llegue el otro jejeje🤍🤍 cafesito caramela, te empezare a decir mi caramelita.",
-        emojis: "🙊🤍❤️‍🔥"  // These will bounce around
+        title: "JEJEJE, Soy la persona mas afortunada de tenerte :D:D🤍🤍",
+        message: "No puedo darte un regalo precensial pero tu sabes que hago lo que puedo, Perdon por lo poco, y tambien por no contestar, estaba haciendo esto lo lamento. era una sorpresa 🤍",
+        emojis: "🌹🤍❤️‍🔥"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -76,7 +76,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dvodywkiv/video/upload/v1771019160/Aquel_Nap_ZzZz_-_Rauw_Alejandro_ending_part_l2ouca.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dvodywkiv/video/upload/v1771043385/Keaton_Henson_-_Small_Hands_dvggzq.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
